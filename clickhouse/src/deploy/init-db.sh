b8e1aae87cfaa807e8040c69eb61307220444946
sergiobemar <sergiobenitomartin@gmail.com>
2021-01-07 04:21:25 +0100

    Updated

--- clickhouse/src/deploy/init-db.sh
+++ clickhouse/src/deploy/init-db.sh
@@ -11,7 +11,16 @@ set -e
 #	CREATE TABLE IF NOT EXISTS raw.test (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8) ENGINE = Memory;
 #EOSQL
 
-#clickhouse-client --format_csv_delimiter="," --query="INSERT INTO raw.meal FORMAT CSVWithNames" < /var/lib/clickhouse/user_files/meal_info.csv
-
+# DATABASE
 clickhouse-client --query="CREATE DATABASE IF NOT EXISTS raw;"
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.meal (meal_id Int64, category String, cuisine String) ENGINE = Memory;"
+
+# TABLES
+## RAW
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.meal (meal_id Int64, category String, cuisine String) ENGINE = Memory"
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.train (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8, num_orders Int32) ENGINE = Memory"
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.test (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8) ENGINE = Memory"
+
+## PROCESSED
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS processed.train (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8, num_orders Int32, city_code Int32, region_code Int32, center_type String, op_area Float32, category String, cuisine String) ENGINE = Memory"
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS processed.test (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8, city_code Int32, region_code Int32, center_type String, op_area Float32, category String, cuisine String) ENGINE = Memory"
