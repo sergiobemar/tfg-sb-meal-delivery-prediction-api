0ac9053d53a23225c2e4d61dd5ee2528a9f25a67
sergiobemar <sergiobenitomartin@gmail.com>
2021-01-11 01:00:58 +0100

    Updated

--- clickhouse/src/deploy/init-db.sh
+++ clickhouse/src/deploy/init-db.sh
@@ -13,10 +13,11 @@ set -e
 
 # DATABASE
 clickhouse-client --query="CREATE DATABASE IF NOT EXISTS raw;"
-clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.meal (meal_id Int64, category String, cuisine String) ENGINE = Memory;"
+clickhouse-client --query="CREATE DATABASE IF NOT EXISTS processed;"
 
 # TABLES
 ## RAW
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.center (center_id Int64, city_code Int32, region_code Int32, center_type String, op_area Float32) ENGINE = Memory"
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.meal (meal_id Int64, category String, cuisine String) ENGINE = Memory"
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.train (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8, num_orders Int32) ENGINE = Memory"
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.test (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8) ENGINE = Memory"
