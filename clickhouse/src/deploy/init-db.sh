5f8a3d14e25b192d2d1363e5a29fa655fe1e34aa
sergiobemar <sergiobenitomartin@gmail.com>
2021-01-12 22:54:27 +0100

    Add center location

--- clickhouse/src/deploy/init-db.sh
+++ clickhouse/src/deploy/init-db.sh
@@ -21,6 +21,8 @@ clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.center (center_id Int6
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.meal (meal_id Int64, category String, cuisine String) ENGINE = Memory"
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.train (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8, num_orders Int32) ENGINE = Memory"
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.test (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8) ENGINE = Memory"
+clickhouse-client --query="CREATE TABLE IF NOT EXISTS raw.center_location (center_id Int64, city String, region String) ENGINE = Memory"
+
 
 ## PROCESSED
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS processed.train (id Int64, week Int16, center_id Int64, meal_id Int64, checkout_price Float32, base_price Float32, emailer_for_promotion Int8, homepage_featured Int8, num_orders Int32, city_code Int32, region_code Int32, center_type String, op_area Float32, category String, cuisine String) ENGINE = Memory"
